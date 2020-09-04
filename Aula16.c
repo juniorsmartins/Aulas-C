e9b2d0c0a3b4d3cf0cdb3ff2d40ccb3d3c9b4b2a
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-03 23:15:12 -0400

    Aula C
    Aula C

--- Aula16.c
+++ Aula16.c
@@ -1,14 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
+#include <stdio.h> // biblioteca padrão de entrada e saída e de definições de constantes, variáveis e tipos.
+#include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
+#include <ctype.h> // biblioteca para classificar caracteres ASCII
 
 int main () 
 {
+  int valor1;
+  int valor2;
+  int total;
 
+  printf("\t Digite o valor 1: ");
+  scanf("%i", &valor1);
 
+  printf("\t Digite o valor 2: ");
+  scanf("%i", &valor2);
 
+  total = valor1 + valor2;
+  printf("\t \n A soma do valor1 + valor2 é: %d ", total);
 
+  system("pause");
 
-
-  return = 0;
+  return 0;
 }
---
+++ Aula17.c
@@ -0,0 +1,40 @@
+#include <stdio.h> // biblioteca padrão de entrada e saída e de definições de constantes, variáveis e tipos.
+#include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
+#include <ctype.h> // biblioteca para classificar caracteres ASCII
+
+void pulaLinha() {
+  printf("\n\n");
+  return 0;
+}
+
+int main() {
+  float nota1;
+  float nota2;
+  float media;
+  nota1 = nota2 = media = 0;
+
+  pulaLinha();
+  printf("Digite a primeira nota: ");
+  scanf("%f", &nota1);
+  pulaLinha();
+  printf("Digite a segunda nota: ");
+  scanf("%f", &nota2);
+  pulaLinha();
+  media = ((nota1 + nota2) / 2);
+  printf("O resultado: %f", media);
+  pulaLinha();
+
+  if (media < 7) {
+    printf("Reprovado!");
+  } else {
+      if (media == 7) || (media == 8) {
+        printf("Aprovado!");
+      } 
+    } else { 
+        if (media == 10) {
+          printf("Aprovado com louvor!");
+        }
+      }
+  pulaLinha();
+  return 0;
+}
