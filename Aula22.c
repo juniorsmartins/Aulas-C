a1b23629b0d2c441fcd2a5b0e5e4bbf1f506a275
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-09 16:04:49 -0400

    Aula C
    Aula C

---
+++ Aula22.c
@@ -0,0 +1,17 @@
+#include <stdio.h> // biblioteca padrão de entrada e saída e de definições de constantes, variáveis e tipos.
+#include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
+#include <ctype.h> // biblioteca para classificar caracteres ASCII
+
+/* 
+  Curso eXcript Develop
+  Operadores 
+*/
+
+int main () {
+
+  int i = 40;
+  printf('%i', (i > 20) && (i < 100));
+
+  return 0;
+};
+
