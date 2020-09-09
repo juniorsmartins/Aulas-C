a72502e3c6950ba03052347bd69cc32ec6e7a11a
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-09 17:52:30 -0400

    Aula C
    Aula C

--- Aula22.c
+++ Aula22.c
@@ -2,13 +2,29 @@
 #include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
 #include <ctype.h> // biblioteca para classificar caracteres ASCII
 
-int main () {
+int main () 
+{
 
-  int i = 40;
-  int condicao = (i>20) && (i<100);
-  printf('%i', condicao);
-  printf('%d', !condicao);
+/* Modificadores de Variáveis */
+
+  int valor1 = 2;
+  printf("%i int \n", sizeof(valor1));
+
+  short int valor2 = 2;
+  printf("%i short int \n", sizeof(valor2));
+
+  unsigned int valor3 = 2;
+  printf("%i unsigned int \n", sizeof(valor3));
+
+  unsigned short int valor4 = 2;
+  printf("%i unsigned short int \n", sizeof(valor4));
+
+  double valor5 = 2;
+  printf("%i double \n", sizeof(valor5));
+
+  long double valor6 = 2;
+  printf("%i long double \n", sizeof(valor6));
 
   return 0;
-}
+};
 
