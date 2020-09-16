49d588d5d2f98367ff99bea61497b398f83cf2c8
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-16 14:10:24 -0400

    Aula C
    Aula C

--- Aula28.c
+++ Aula28.c
@@ -2,6 +2,25 @@
 #include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
 #include <ctype.h> // biblioteca para classificar caracteres ASCII
 
+void pulaLinha()
+{
+  printf("\n");
+}
 
+int main ()
+{
+  printf("Vídeo-aula no Youtube - Canal eXcript")
+  int contador = 0;
+  int valDigitado = 0;
+  pulaLinha();
+  printf("Digite um valor inteiro: ");
+  scanf("%i", &valDigitado);
+  while (contador <= valDigitado)
+  {
+    printf("%i x %i = %i", contador, valDigitado, contador * valDigitado);
+    contador++;
+    pulaLinha();
+  }
 
-
+  return 0;
+}
