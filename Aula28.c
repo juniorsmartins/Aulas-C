81bf88f381d5a5f70953907ed75f3e918bfee146
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-16 13:51:22 -0400

    Aula C
    Aula C

--- Aula28.c
+++ Aula28.c
@@ -2,23 +2,6 @@
 #include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
 #include <ctype.h> // biblioteca para classificar caracteres ASCII
 
-void pulaLinha ()
-{
-  printf("\n");
-  return 0;
-};
-
-int main ()
-{
-
-  int valor1, valor2, valor3;
-  valor1 = valor2 = valor3 = 2;
-  printf("Os valores: %i, %i e %d", valor1, valor2, valor3);
-  pulaLinha();
-  printf("Variavel de %d bytes", sizeof valor1);
-
-  return 0;
-}
 
 
 
