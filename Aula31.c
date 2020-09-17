1903bfa8b96048a517db6865b1e8b4b1872588cc
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-17 10:11:36 -0400

    Aula C
    Aula C

---
+++ Aula31.c
@@ -0,0 +1,30 @@
+#include <stdio.h> // biblioteca padrão de entrada e saída e de definições de constantes, variáveis e tipos.
+#include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
+#include <ctype.h> // biblioteca para classificar caracteres ASCII
+/*
+  Linguagem de Programação C
+  Canal: eXcript
+  Aula: Do-While
+*/
+void pula_Linha() 
+{
+  printf("\n");
+}
+
+int main ()
+{
+  int contador = 0;
+  char c;
+  do
+  {
+    system("cls");
+    pula_Linha();
+    printf("Digite zero para sair do looping: ");
+    printf("%i", contador);
+    pula_Linha();
+    c = getchar();
+    contador +=1;
+  } while (c != '0');
+  return 0;
+}
+
