852de07a8307f516616242a796ec842f53cbccf0
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-18 11:32:52 -0400

    Aula C
    Aula C

--- Aula30-Lista3.c
+++ Aula30-Lista3.c
@@ -36,7 +36,7 @@ int exercicio_1()
     system("cls");
     printf("*********************\n");
     printf("**** Exercicio 1 ****\n");
-    printf("*********************\n");
+    printf("*********************");
     pula_Linha();
     printf("Escreva um algoritmo para ler três valores inteiros e escrever na tela o maior e o menor. Considere que todos os valores são diferentes.");
     pula_Linha();
@@ -44,48 +44,51 @@ int exercicio_1()
     valor1 = valor2 = valor3 = 0;
     printf("Digite o primeiro valor: ");
     scanf("%i", &valor1);
-    printf("\nDigite o segundo valor: ");
+    pula_Linha();
+    printf("Digite o segundo valor: ");
     scanf("%i", &valor2);
-    printf("\nDigite o terceiro valor: ");
+    pula_Linha();
+    printf("Digite o terceiro valor: ");
     scanf("%d", &valor3);
+    pula_Linha();
     if (valor1 != valor2 && valor1 != valor3 && valor2 != valor3)
     {
       if (valor1 > valor2 && valor1 > valor3)
       {
-        pula_Linha();
         printf("O maior valor: %i", valor1);
+        pula_Linha();
       }
       if (valor2 > valor1 && valor2 > valor3)
       {
-        pula_Linha();
         printf("O maior valor: %i", valor2);
+        pula_Linha();
       }
       if (valor3 > valor1 && valor3 > valor2)
       {
-        pula_Linha();
         printf("O maior valor: %i", valor3);
+        pula_Linha();
       }
       if (valor1 < valor2 && valor1 < valor3)
       {
-        pula_Linha();
         printf("O menor valor: %i", valor1);
+        pula_Linha();
       }
       if (valor2 < valor1 && valor2 < valor3)
       {
-        pula_Linha();
         printf("O menor valor: %d", valor2);
+        pula_Linha();
       }
       if (valor3 < valor1 && valor3 < valor2)
       {
-        pula_Linha();
         printf("O menor valor: %i", valor3);
+        pula_Linha();
       }
       resposta = 1;
     }
     else 
     {
-      pula_Linha();
       printf("Errado! Digitou valores iguais. Refaça!");
+      pula_Linha();
       resposta = 2;
     }
   } while (resposta != 1);
@@ -103,44 +106,46 @@ int exercicio_2()
   system("cls");
   printf("*********************\n");
   printf("**** Exercicio 2 ****\n");
-  printf("*********************\n");
+  printf("*********************");
   pula_Linha();
   printf("Escrever um algoritmo que leia valores inteiros em duas variáveis distintas.\n");
   printf("1) Se o resto da divisão da primeira pela segunda for 1, mostre a soma dessas variáveis mais o resto da divisão;\n");
   printf("2) Se o resto da divisão da primeira pela segunda for igual a três, multiplique a soma dos valores lidos pelo primeiro;\n");
   printf("3) Se o resto da divisão da primeira pela segunda for igual a quatro, divida a soma dos números lidos pelo segundo, se este for diferente de zero;\n");
-  printf("4) Em qualquer outra situação, mostre o quadrado dos números lidos.\n");
+  printf("4) Em qualquer outra situação, mostre o quadrado dos números lidos.");
   pula_Linha();
   printf("Digite o primeiro valor inteiro: ");
   scanf("%i", &entrada1);
-  printf("\nDigite o segundo valor inteiro: ");
+  pula_Linha();
+  printf("Digite o segundo valor inteiro: ");
   scanf("%i", &entrada2);
+  pula_Linha();
   resto_Deles = (entrada1 % entrada2);
   if (resto_Deles == 1)
   {
     resultado = (entrada1 + entrada2 + resto_Deles);
+    printf("Condição 1 atendida!");
     pula_Linha();
-    printf("Condição 1 atendida!\n");
     printf("A soma + resto é: %i", resultado);
   } 
   else if (resto_Deles == 3)
   {
     resultado = ((entrada1 + entrada2) * entrada1);
+    printf("Condição 2 atendida!");
     pula_Linha();
-    printf("Condição 2 atendida!\n");
     printf("A multiplicação da soma pelo primeiro é: %i", resultado);
   }
   else if (resto_Deles == 4 && entrada2 != 0)
   {
     resultado = ((entrada1 + entrada2) / entrada2);
+    printf("Condição 3 atendida!");
     pula_Linha();
-    printf("Condição 3 atendida!\n");
     printf("A divisão da soma pelo segundo, se este for diferente de zero, é: %i", resultado);
   }
   else  
   {
+    printf("Condição 4 atendida!");
     pula_Linha();
-    printf("Condição 4 atendida!\n");
     printf("O quadrado dos números lidos é: %i e %i", entrada1 * entrada1, entrada2 * entrada2);
   }
   pula_Linha();
@@ -155,10 +160,9 @@ int exercicio_3()
   system("cls");
   printf("*********************\n");
   printf("**** Exercicio 3 ****\n");
-  printf("*********************\n");
+  printf("*********************");
   pula_Linha();
-  printf("Escreva um algoritmo que leia três valores para os lados de um triângulo. ");
-  printf("E depois diga se o triângulo é equilátero, isósceles ou escaleno.");
+  printf("Escreva um algoritmo que leia três valores para os lados de um triângulo. E diga se o triângulo é equilátero, isósceles ou escaleno. \n");
   pula_Linha();
   printf("Digite o primeiro lado de um triângulo: ");
   scanf("%f", &tri1);
@@ -169,25 +173,25 @@ int exercicio_3()
   pula_Linha();
   if (tri1 == tri2 && tri1 == tri3)
   {
-    printf("\nTriângulo Equilátero - Todos os lados iguais!");
+    printf("Triângulo Equilátero - Todos os lados iguais!");
   }
   else if (tri1 != tri2 && tri1 != tri3 && tri2 != tri3)
   {
-    printf("\nTriângulo Scaleno - Todos os lados diferentes!");
+    printf("Triângulo Scaleno - Todos os lados diferentes!");
   }
   else
   {
     if (tri1 == tri2 && tri1 != tri3)
     {
-      printf("\nTriângulo Isósceles - Dois lados iguais!");
+      printf("Triângulo Isósceles - Dois lados iguais!");
     }
     if (tri1 == tri3 && tri1 != tri2)
     {
-      printf("\nTriângulo Isósceles - Dois lados iguais!");
+      printf("Triângulo Isósceles - Dois lados iguais!");
     }
     if (tri2 == tri3 && tri2 != tri1)
     {
-      printf("\nTriângulo Isósceles - Dois lados iguais!");
+      printf("Triângulo Isósceles - Dois lados iguais!");
     }
   }
   pula_Linha();
@@ -202,11 +206,11 @@ int exercicio_4()
   system("cls");
   printf("*********************\n");
   printf("**** Exercicio 4 ****\n");
-  printf("*********************\n");
+  printf("*********************");
   pula_Linha();
-  printf("Escreva um algoritmo que leia um número inteiro.");
-  printf("\n1) Se o número lido for positivo, escreva uma mensagem indicando se é par um ímpar;");
-  printf("\n2) Se o número for negativo, escreva a seguinte mensagem 'Este número não é positivo'.");
+  printf("Escreva um algoritmo que leia um número inteiro. \n");
+  printf("1- Se o número lido for positivo, escreva uma mensagem indicando se é par um ímpar; \n");
+  printf("2- Se o número for negativo, escreva a seguinte mensagem: 'Este número não é positivo'.");
   pula_Linha();
   printf("Digite um valor inteiro qualquer: ");
   scanf("%i", &valor_Inteiro);
@@ -242,7 +246,7 @@ int exercicio_5()
   system("cls");
   printf("*********************\n");
   printf("**** Exercicio 5 ****\n");
-  printf("*********************\n");
+  printf("*********************");
   pula_Linha();
   printf("Elabora um algoritmo que informe se um dado ano é ou não bissexto.\n");
   printf("Obs: é bissexto se for divisível por 400 ou se for divisível por 4 e não por 100.");
@@ -267,38 +271,38 @@ int exercicio_5()
 
 int exercicio_6()
 {
-  char nome6[40];
+  char nome_Seis[30];
   int dia, mes, contador1;
   dia = mes = contador1 = 0;
   system("cls");
   printf("*********************\n");
   printf("**** Exercicio 6 ****\n");
-  printf("*********************\n");
+  printf("*********************");
   pula_Linha();
-  printf("Verifique a validade de uma data de aniversário (solicite apenas o número do dia e do mês)\n.");
-  printf("1- Solite o nome e a idade de duas pessoas;\n");
-  printf("2- Além de falar se a data está ok, informe também o nome do mês\n.");
+  printf("Verifique a validade de uma data de aniversário (solicite apenas o número do dia e do mês). \n");
+  printf("1- Solite o nome e a idade de duas pessoas; \n");
+  printf("2- Além de falar se a data está ok, informe também o nome do mês. \n");
   printf("Dica: meses com 30 dias: abril, junho, setembro e novembro.");
-  pula_Linha();
   for (contador1 = 0; contador1 < 2; contador1++) 
   {
     pula_Linha();
     printf("Qual o nome do usuário? ");
-    fgets(nome6, 40, stdin);
+    scanf("%s", nome_Seis);
     printf("\nQual o dia do teu aniversário? ");
     scanf("%i", &dia);
-    printf("\nQual o número do mês do teu aniversário? ");
+    printf("\nQual o mês do teu aniversário (1 a 12)? ");
     scanf("%i", &mes);
+    pula_Linha();
     if (mes == 1)
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Janeiro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -306,12 +310,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 30)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Fevereiro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -319,12 +323,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Março.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -332,12 +336,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 31)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Abril.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -345,12 +349,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Maio.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -358,12 +362,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 31)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Junho.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -371,12 +375,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Julho.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -384,12 +388,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Agosto.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -397,12 +401,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 31)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Setembro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -410,12 +414,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Outubro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -423,12 +427,12 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 31)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Novembro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
@@ -436,18 +440,18 @@ int exercicio_6()
     {
       if (dia > 0 && dia < 32)
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Aniversário no dia %i de Dezembro.", dia);
       }
       else
       {
-        printf("%c \n", nome6);
+        printf("%s \n", nome_Seis);
         printf("Data Inválida!");
       }
     }
     else if (mes < 1 || mes > 12)
     {
-      printf("%c \n", nome6);
+      printf("%s \n", nome_Seis);
       printf("Data Inválida!");
     }
   }
