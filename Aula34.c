44239b3558731da4835434da25d396b6546eda94
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-23 14:12:37 -0400

    Aula C
    Aula C

--- Aula34.c
+++ Aula34.c
@@ -5,16 +5,38 @@
 
 int main()
 {
+  int cont1, cont2, ab, cd, tempo;
   printf("\n");
   printf("LP C");
-  printf("\n");  
-
-  int x;
-  for (x = 1; x < 6; x++)
+  
+  printf("\n\n");
+  printf("For Padrão Crescente");
+  for (cont1 = 1; cont1 < 6; cont1++)
   {
     printf("\n");
-    printf("%i", x);
+    printf("%i", cont1);
   }
+
+  printf("\n\n");
+  printf("For Padrão Decrescente");
+  for (cont2 = 5; cont2 >= 1; cont2--)
+  {
+    printf("\n");
+    printf("%d", cont2);
+  }
+
+  printf("\n\n");
+  printf("Variação For\n");
+  for (ab = 0, cd = 3; ab + cd <= 10; ab++, cd++)
+  {
+    printf("ab = %i e cd = %d e a soma é: %i \n", ab, cd, ab+cd);
+  }
+
+  printf("\n\n");
+  printf("Laço For sem corpo\n");
+  for (tempo = 0; tempo <= 500000; tempo++);
+  printf("Tempo Encerrado!");
+
 
   return 0;
 }
