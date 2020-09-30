a39f346526465a690354626119c6603517684b19
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-30 16:17:08 -0400

    Aula C
    Aula C

--- Aula41.c
+++ Aula41.c
@@ -3,7 +3,29 @@
 #include <ctype.h> // biblioteca para classificar caracteres ASCII
 #include <string.h> // biblioteca com funções para manipular strings
 
+int main()
+{
+  system("cls");
+  char alfabeto;
+  char palavra[10];
+  char frase[40];
 
+  puts("Digite uma letra: ");
+  alfabeto = getchar();
 
+  puts("Digite uma palavra: ");
+  scanf("%s", &palavra);
 
+  puts("Digite uma frase: ");
+  scanf("%s", frase);
+
+  puts("\n");
+  putchar(alfabeto);
+  puts("\n");
+  puts(palavra);
+  puts("\n");
+  printf("%s", frase);
+
+  return 0;
+};
 
