f86c52baafb7edd048682db50662fba4b3ffb197
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-03 23:27:28 -0400

    Aula C
    Aula C

--- Aula32-Vendas.c
+++ Aula32-Vendas.c
@@ -1,6 +1,6 @@
 #include <stdio.h> // biblioteca padrão de entrada e saída e de definições de constantes, variáveis e tipos.
 #include <stdlib.h> // biblioteca padrão de alocação de memória, controle de processos e conversões, entre outros. 
-#include <ctype.h> // biblioteca para classificar caracteres ASCII
+#include <ctype.h> // biblioteca de funções para conversão de maiúsculas, minúsculas e outros tratamentos de caracteres ASCII.
 #include <string.h> // biblioteca com funções para manipular strings
 #define VERSAO "1.0";
 unsigned int contdor;
