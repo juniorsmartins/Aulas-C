0fb83d9f937fa46047a607ce9486c4955641b16d
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-27 12:49:02 -0400

    Treino BackEnd
    Treino BackEnd

--- Prova2sem/Exercicio_1.c
+++ Prova2sem/Exercicio_1.c
@@ -44,7 +44,20 @@ int menu_Principal()
 
 
 // ----- Início da Seção de Exercícios ----- //
+  // -- Início do Exercício 1 -- //
+  void exercicio_1()
+  {
+    system("cls");
+    printf("\n \n \t Exercicio 1 \n \n \n");
+    system("pause");
+  }
+  // -- Fim do Exercício 1 -- //
+  // -- Início do Exercício 2 -- //
 
+  // -- Fim do Exercício 2 -- //
+  // -- Início do Exercício 3 -- //
+
+  // -- Fim do Exercício 3 -- //
 // ----- Fim da Seção de Exercícios ----- //
 
 
@@ -58,55 +71,106 @@ int main ()
     opcao_Principal = menu_Principal();
     switch (opcao_Principal)
     {
+    case 0:
+      /* Sair = 0 */
+      system("cls");
+      printf("\n \t Tchau, amiguinho. \n");
+      printf("\n \t Volte Sempre! \n \n \n");
+      system("pause");
+      break;
     case 1:
       /* Exercício 1: */
+      exercicio_1();
       break;
     case 2:
       /* Exercício 2: */
+      system("cls");
+      printf("\n \n \t Exercicio 2 \n \n \n");
+      system("pause");
       break;
     case 3:
       /* Exercício 3: */
+      system("cls");
+      printf("\n \n \t Exercicio 3 \n \n \n");
+      system("pause");
       break;
     case 4:
       /* Exercício 4: */
+      system("cls");
+      printf("\n \n \t Exercicio 4 \n \n \n");
+      system("pause");
       break;
     case 5:
       /* Exercício 5: */
+      system("cls");
+      printf("\n \n \t Exercicio 5 \n \n \n");
+      system("pause");
       break;
     case 6:
       /* Exercício 6: */
+      system("cls");
+      printf("\n \n \t Exercicio 6 \n \n \n");
+      system("pause");
       break;
     case 7:
       /* Exercício 7: */
+      system("cls");
+      printf("\n \n \t Exercicio 7 \n \n \n");
+      system("pause");
       break;
     case 8:
       /* Exercício 8: */
+      system("cls");
+      printf("\n \n \t Exercicio 8 \n \n \n");
+      system("pause");
       break;
     case 9:
       /* Exercício 9: */
+      system("cls");
+      printf("\n \n \t Exercicio 9 \n \n \n");
+      system("pause");
       break;
     case 10:
       /* Exercício 10: */
+      system("cls");
+      printf("\n \n \t Exercicio 10 \n \n \n");
+      system("pause");
       break;
     case 11:
       /* Exercício 11: */
+      system("cls");
+      printf("\n \n \t Exercicio 11 \n \n \n");
+      system("pause");
       break;
     case 12:
       /* Exercício 12: */
+      system("cls");
+      printf("\n \n \t Exercicio 12 \n \n \n");
+      system("pause");
       break;
     case 13:
       /* Exercício 13: */
+      system("cls");
+      printf("\n \n \t Exercicio 13 \n \n \n");
+      system("pause");
       break;
     case 14:
       /* Exercício 14: */
+      system("cls");
+      printf("\n \n \t Exercicio 14 \n \n \n");
+      system("pause");
       break;
     case 15:
       /* Exercício 15: */
+      system("cls");
+      printf("\n \n \t Exercicio 15 \n \n \n");
+      system("pause");
       break;
     default:
       /* Opção Inválida! */
       system("cls");
-      printf("\n \t Tente novamente! \n \n");
+      printf("\n \t Opcao Invalida! \n");
+      printf("\n \t Tente novamente! \n \n \n");
       system("pause");
       break;
     }
